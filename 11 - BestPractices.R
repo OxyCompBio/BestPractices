5bc2108cb57d5cc2880aa5eec5566d44f15bec4c
iriszbibi <60709075+iriszbibi@users.noreply.github.com>
2020-02-14 14:34:16 -0800

    Update 11 - BestPractices.R
    Iris Camille edits for best practices

--- 11 - BestPractices.R
+++ 11 - BestPractices.R
@@ -1,32 +1,11 @@
 N <- 10
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-B <- round(rnorm(1, 3, 1), 0)
-D <- round(rnorm(1, 3, 1), 0)
-N <- N + B - D
-print(N)
+for( i in 1:10){
+  B <- round(rnorm(1, 3, 1), 0) # random number of births with a mean of 3
+  D <- round(rnorm(1, 3, 1), 0) # random number of deaths with a mean of 3
+  if(N <= 0 | (N+B-D) <= 0){ # if the total population becomes negative 
+    N <- 0 # change the total population to 0
+    } else{ # if the total population is positive
+    N <- N+B-D # continue to calculate the total population 
+    }
+  print(N) # print out the final population
+  }
